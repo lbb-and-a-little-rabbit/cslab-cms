cf9d806847b09882547a54dd1ff2b2de57ca87c3
lbb-and-a-little-rabbit <915598980@qq.com>
2025-12-04 13:57:30 +0800

    function1

--- funcptr.cpp
+++ funcptr.cpp
@@ -20,10 +20,20 @@ FUNC funcptr=add;
 
 int (*op[2])(int,int)={add,sub};
 
+void run(void (*func)()){
+    func();
+}
+
+void test(){
+    cout << "hello World\n";
+}
+
 int main(){
     int r1=fp(1,2);
     int r2=(*fp)(1,2);
     cout << r1 << ' ' << r2 << '\n';// 3 3 
     cout << funcptr(3,4) << ' ' << (*funcptr)(3,4) << '\n';// 7 7
     cout << op[0](5,6) << ' ' << (*op[1])(5,6) << '\n';//11 -1
+    void (*f)()=test;
+    run(f);// "hello World"
 }
